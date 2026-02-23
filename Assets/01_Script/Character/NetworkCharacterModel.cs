75032836b353c4ad3d049f94adbd6a91de097633
aincon <coran4178@gmail.com>
2026-02-23 22:06:21 +0900

    애니메이션 연결
    연공, 강공, 피격, 사망(포지션 제어 필요)

--- Assets/01_Script/Character/NetworkCharacterModel.cs
+++ Assets/01_Script/Character/NetworkCharacterModel.cs
@@ -4,19 +4,28 @@ using System;
 
 public class NetworkCharacterModel : NetworkBehaviour
 {
-    [SyncVar(hook = nameof(OnComboChangedHook))] 
+    [SyncVar(hook = nameof(OnComboChangedHook))]
     private int comboCount = 0;
-    
-    [SyncVar(hook = nameof(OnChargeStateChangedHook))] 
+
+    [SyncVar(hook = nameof(OnChargeStateChangedHook))]
     private bool isCharging = false;
+
+    [SyncVar(hook = nameof(OnHealthChangedHook))]
+    public float currentHealth = 100f;
+
+    [SyncVar(hook = nameof(OnDieHook))] 
+    private bool isDead = false;
 
     public int ComboCount => comboCount;
     public bool IsCharging => isCharging;
-    public bool IsDead => false; // 임시
+    public float CurrentHealth => currentHealth;
+    public bool IsDead => isDead;
 
     public event Action<int> OnComboChanged;
+    public event Action OnDie;
     public event Action<bool> OnChargeStateChanged;
     public event Action OnStrongAttack;
+    public event Action<float> OnHealthChanged;
 
     [Command]
     public void CmdNextCombo() { comboCount = (comboCount % 3) + 1; }
@@ -30,9 +39,32 @@ public class NetworkCharacterModel : NetworkBehaviour
     [Command]
     public void CmdStrongAttack() { RpcPlayStrongAttack(); }
 
+    [Command(requiresAuthority = false)]
+    public void CmdTakeDamage(float damageAmount)
+    {
+        if (IsDead) return;
+
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            isDead = true;
+        }
+    }
+
     [ClientRpc]
     void RpcPlayStrongAttack() { OnStrongAttack?.Invoke(); }
 
     void OnComboChangedHook(int oldV, int newV) => OnComboChanged?.Invoke(newV);
     void OnChargeStateChangedHook(bool oldV, bool newV) => OnChargeStateChanged?.Invoke(newV);
+    void OnHealthChangedHook(float oldV, float newV) => OnHealthChanged?.Invoke(newV);
+    void OnDieHook(bool oldV, bool newV)
+    {
+        if (newV == true && oldV == false) 
+        {
+            OnDie?.Invoke();
+        }
+    }
+
 }
