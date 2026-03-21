be1dad4bb025e678daa066a10501ff153444486e
seongjpg <dodoboary@gmail.com>
2026-03-21 21:22:44 +0900

    단검제작
    ㅇ

---
+++ Assets/01_Script/Item/Weapon/Dagger/WeaponDagger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDagger : MonoBehaviour
+{
+
+    [Header("아이템 정보")]
+    [SerializeField] public ItemStatus itemStat;
+
+    private float lifeTimer;
+    private void Update()
+    {
+        lifeTimer += Time.deltaTime;
+        if (lifeTimer > itemStat.availableTime)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        if (Input.GetKeyDown("V"))
+        {
+            ThrowDagger();
+        }
+    }
+
+    private void ThrowDagger()
+    {
+        Debug.Log("단검 던지기!");
+    }
+}
